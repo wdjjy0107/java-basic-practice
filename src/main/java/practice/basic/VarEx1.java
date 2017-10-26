fec852086c6c6ce327233aa69a343300d6c8ccbc
wdjjy0107 <wdjjy0107@gmail.com>
2017-10-26 12:56:45 +0900

    init

--- src/main/java/practice/basic/VarEx1.java
+++ src/main/java/practice/basic/VarEx1.java
@@ -1,7 +1,7 @@
 package practice.basic;
 
 public class VarEx1 {
-    public static void main(String args) {
+    public static void main(String[] args) {
         int year = 0;
         int age = 14;
 
